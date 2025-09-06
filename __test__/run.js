040ce9029acdf3b988d7489a7b3b4d9b16d9c102
ziaurrehmanii <ziaurrehmanrehmani@gmail.com>
2025-09-07 00:39:36 +0500

    Update test cases in run.js to include various string and comment scenarios

--- __test__/run.js
+++ __test__/run.js
@@ -6,10 +6,21 @@ const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
 const testProgram = [
+  //Numbers
   `123`,
-  `            123`,
-  `"Hello, World!"`,
-  `'Hello, World!'`,
+  //Whitespace should be ignored
+  `            "Whitespace before"`,
+  // Double quoted strings
+  `"Double quoted string"`,
+  // Single quoted strings
+  `'Single quoted string'`,
+  // Single line comment should be ignored
+  `// This is a comment
+    "Single line comment"`,
+  // Multi line comment should be ignored
+  `/* This is a 
+       multi line comment */
+    "Multi line comment"`,
 ];
 for (let i = 0; i < testProgram.length; i++) {
   const program = testProgram[i];
